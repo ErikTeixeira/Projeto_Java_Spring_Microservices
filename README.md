e51d2fb03f2ed42bf035a5bb54be8699f36ba2ad
ErikTeixeira <erik.barbosa515@gmail.com>
2025-06-16 18:35:38 -0300

    User Microservice controller and service

---
+++ README.md
@@ -0,0 +1,10 @@
+## Post
+
+- http://localhost:8081/users
+
+```java
+{
+    "name": "Erik",
+    "email": "erikteixeirabarbosa@gmail.com"
+}
+```
--- user/src/main/java/com/ms/user/controller/UserController.java
+++ user/src/main/java/com/ms/user/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.ms.user.controller;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
+import com.ms.user.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @PostMapping("/users")
-    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto UserRecordDto) {
+    @Autowired
+    UserService userService;
 
-        return ResponseEntity.status(HttpStatus.CREATED).body();
+    @PostMapping("/users")
+    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto) {
+
+        // converter o record em model
+        var userModel = new UserModel();
+        BeanUtils.copyProperties( userRecordDto, userModel );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body( userService.save(userModel) );
     }
 
 }
